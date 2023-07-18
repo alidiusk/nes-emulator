f31c66ff803f56a1a8bfdfe26404a6d2d51f0a45
Maya Woodward <mayawoodward0@gmail.com>
2023-07-18 17:39:16 -0400

    Add registers to cpu impl

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,27 +1,44 @@
 
 
-pub type CpuError = Result<(), ()>;
+pub type CpuResult = Result<(), ()>;
 
+#[derive(Debug, Clone, PartialEq, Default)]
+struct ByteRegister(u8);
+
+#[derive(Debug, Clone, PartialEq, Default)]
+struct HalfWordRegister(u16);
+
+#[derive(Debug, Clone, PartialEq, Default)]
+pub struct Registers {
+    pc: HalfWordRegister,
+    sp: ByteRegister,
+    p: ByteRegister,
+    a: ByteRegister,
+    x: ByteRegister,
+    y: ByteRegister,
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct Cpu {
-
+    registers: Registers,
 }
 
 impl Cpu {
     pub fn new() -> Cpu {
         Cpu {
-
+            registers: Registers::default(),
         }
     }
 
-    pub fn startup(&mut self) -> CpuError {
+    pub fn startup(&mut self) -> CpuResult {
         Ok(())
     }
 
-    pub fn shutdown(&mut self) -> CpuError {
+    pub fn shutdown(&mut self) -> CpuResult {
         Ok(())
     }
 
-    pub fn tick(&mut self) -> CpuError {
+    pub fn tick(&mut self) -> CpuResult {
         Ok(())
     }
 
